12a03592bb18c6e117e552f3308d69c79a3166ce
kellynvd <kellynvd@gmail.com>
2019-12-12 11:46:59 -0300

    Add Edit and Destroy actions and view files

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
   before_action :find_play
+  before_action :find_review, only: %i[edit update destroy]
 
   def new
     @review = Review.new
@@ -17,10 +18,30 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @review.update(review_params)
+      redirect_to play_path(@play)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @review.destroy
+    redirect_to play_path(@play)
+  end
+
   private
 
   def find_play
     @play = Play.find(params[:play_id])
+  end
+
+  def find_review
+    @review = Review.find(params[:id])
   end
 
   def review_params
